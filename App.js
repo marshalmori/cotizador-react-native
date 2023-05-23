f961dfc405995bafb4bedf396537dfeb8852fc36
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-05-23 09:57:27 -0300

    Inserir imagem abaixo do Header

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet, Image} from 'react-native';
 import Header from './components/Header';
 
 const App = () => {
   return (
     <>
       <Header />
+
+      <Image
+        style={styles.imagen}
+        source={require('./assets/img/cryptomonedas.png')}
+      />
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imagen: {
+    width: '100%',
+    height: 150,
+    marginHorizontal: '2.5%',
+  },
+});
 
 export default App;
