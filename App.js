1ec8f3591e6a33d93379e3a9a1ee0740788cf9fa
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-05-23 08:59:40 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return <></>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
