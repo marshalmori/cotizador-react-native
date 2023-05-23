fa0c967fdc931b1f2b210ab25b75e77afa6469d3
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-05-23 10:33:14 -0300

    Criar no formulário um Picker para escolha da moeda

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 const Formulario = () => {
   return (
     <View>
       <Text style={styles.label}>Moneda</Text>
+      <Picker>
+        <Picker.Item label="-- Seleccione --" value=""></Picker.Item>
+        <Picker.Item label="Dolar Americano" value="USD"></Picker.Item>
+        <Picker.Item label="Peso Mexicano" value="MXN"></Picker.Item>
+        <Picker.Item label="Euro" value="EUR"></Picker.Item>
+        <Picker.Item label="Libra Esterlina" value="GBP"></Picker.Item>
+        <Picker.Item label="Real" value="BRL"></Picker.Item>
+      </Picker>
 
       <Text style={styles.label}>Criptomoneda</Text>
     </View>
